7119694696027f51fb54309a71a5acccebe578cc
Ben <bigben.hunt@gmail.com>
2021-02-02 20:43:09 -0700

    Can run interactive programs and get full compilation errors

--- python/vim_runner.py
+++ python/vim_runner.py
@@ -1,20 +1,18 @@
 """
-A plugin to run a single c/c++ or python file
+A plugin to run a single c/c++ or python file.
+
+Inspired by VSCode's Code Runner this aims to emulate accomplish similar things
+but at a far smaller scale.  Currently it only supports C/C++ and python but it can
+be extended further if need be.
 """
 import os
-import re
 from typing import Tuple
 from enum import Enum
 
 import vim
 
-# regex
-CPP_EXT1 = "\.cpp$"
-CPP_EXT2 = "\.cc$"
-C_EXT = "\.c$"
-PY_EXT = "\.py$"
-
-LOG_FILE = "/home/ben/repos/vim-runner/logfile.txt"
+C_COMPILER = 'gcc'
+CPP_COMPILER = 'g++'
 
 
 class FileType(Enum):
@@ -32,7 +30,6 @@ def __determine_file_type() -> Tuple[FileType, str]:
     :returns
         filetype(FileType): the type of file
     """
-
     filetype: str = vim.eval('&filetype')
 
     if filetype == 'python':
@@ -44,77 +41,56 @@ def __determine_file_type() -> Tuple[FileType, str]:
     return FileType.other, filetype
 
 
-# def __run_commmand_in_term(cmd: str) -> None:
-#     """will send cmd to be run in the terminal"""
-#     if vim.eval("has('vim')"):
-#         # call vim specific terminal stuff
-#         pass
-#     else:
-#         # call nvim specific terminal commands
-#         # TODO get the terminal buffer and then do this stuff
-#         vim.command("normal! echo 'trying to print to the terminal'")
-#         vim.command(f'noraml! let @z = "{cmd}\n" | normal! "zp ')
-
-
-def __open_new_display_win(binaryname: str) -> None:
-    """ open a new terminal window 1/3 the height of the original"""
+def __open_new_display_win(command: str) -> None:
+    """
+    open a new terminal window 1/3 the height of the original
+    :params
+        command: the command to run in the terminal
+    """
     win_height = vim.current.window.height
     new_win_height = win_height / 3
-    vim.command(str(new_win_height) + "sp | term " + binaryname)
+    vim.command(str(new_win_height) + "sp | term " + command)
 
 
 def __make_binary_name(filepath: str):
+    """
+    creates the binary name by removing the file extension
+    if the file is found /home/jondo/program.cc then it returns /home/jondo/program
+    :params
+        filepath: the full path to the file
+    """
     return filepath.split('.')[0]
 
 
 def __runner_cmd(navigate: str, build: str, run: str):
+    """
+    creates a run command for the current buffer
+    :params
+        navigate: the command to go to the directory that has the file
+        build: the specific build command for the file
+        run: the command to execute binary
+    """
     cmd = f"{navigate} && {build} && {run}"
     return cmd
 
 
-def __maybe_error_out(result: int) -> None:
-    # TODO figure out how to tell user compilation failed
-    pass
-
-
-def __compile_c_file(filepath: str) -> None:
-    """
-    compile a C file and run it non-interactively
-    :params
-        filepath: the full path to the C file
-    """
-    gcc = "gcc"
-    binaryname = __make_binary_name(filepath)
-    directory = os.path.dirname(filepath)
-
-    navigate = f"cd {directory}"
-    build = f"{gcc} {filepath} -o {binaryname}"
-    run = f"./{binaryname}"
-
-    cmd = __runner_cmd(navigate, build, run)
-    result = os.system(cmd)
-    __maybe_error_out(result)
-    __open_new_display_win(binaryname)
-
-
-def __compile_cpp_file(filepath: str) -> None:
+def __compile_c_cpp_file(compiler_to_use: str, filepath: str) -> None:
     """
     compile a c++ file and run it non-interactively
     :params
+        compiler: the compiler you wish to use (should be one of gcc, g++ or clang)
         filepath: the full path to the C++ file
     """
-    gpp = "g++"
+    compiler = compiler_to_use
     binaryname = __make_binary_name(filepath)
     directory = os.path.dirname(filepath)
 
     navigate = f"cd {directory}"
-    build = f"{gpp} {filepath} -o {binaryname}"
-    run = f"./{binaryname}"
+    build = f"{compiler} {filepath} -o {binaryname}"
+    run = f"{binaryname}"
 
     cmd = __runner_cmd(navigate, build, run)
-    result = os.system(cmd)
-    __maybe_error_out(result)
-    __open_new_display_win(binaryname)
+    __open_new_display_win(cmd)
 
 
 def __run_python_script(filepath: str) -> None:
@@ -131,15 +107,16 @@ def __run_python_script(filepath: str) -> None:
 def compile_current_file() -> None:
     """compile/run the current vim buffer if possible"""
     filepath = vim.current.buffer.name
+
     if filepath is None or filepath == "":
         raise Exception(f"{filepath} is None")
 
     filetype, ft_str = __determine_file_type()
 
     if filetype is FileType.c:
-        __compile_c_file(filepath)
+        __compile_c_cpp_file(C_COMPILER, filepath)
     elif filetype is FileType.cpp:
-        __compile_cpp_file(filepath)
+        __compile_c_cpp_file(CPP_COMPILER, filepath)
     elif filetype is FileType.py:
         __run_python_script(filepath)
     else:
