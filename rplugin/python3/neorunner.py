f721992ef8bd9ba7cbaeb22c9bd4236d508fd208
Ben <bigben.hunt@gmail.com>
2021-02-21 19:03:04 -0700

    Refactor properties

--- rplugin/python3/neorunner.py
+++ rplugin/python3/neorunner.py
@@ -6,6 +6,7 @@ but at a far smaller scale.
 import os
 
 import pynvim
+from typing import Tuple
 
 
 @pynvim.plugin
@@ -15,13 +16,33 @@ class NeoRunner():
     def __init__(self, vim) -> None:
         self.vim = vim
 
+    def __get_var_or_default(self, var: str, default: str) -> str:
+        """
+        Get global variable or a default
+        :params
+            var: the user defined variable like `runner_c_compiler`
+            default: the defualt to use if there is no user defined global variable
+
+        :return either the global variable specified by `var` or `default`
+        """
+        try:
+            return self.vim.api.get_var(var)
+        except:
+            return default
+
+    @property
+    def filepath(self) -> str:
+        """the filepath of the current file"""
+        return self.vim.current.buffer.name
+
+    @property
+    def filetype(self) -> str:
+        return self.vim.eval('&filetype')
+
     @property
     def c_compiler(self) -> str:
         """return the user defined C compiler or the default if none exist"""
-        try:
-            return self.vim.api.get_var('runner_c_compiler')
-        except:
-            return 'gcc'
+        return self.__get_var_or_default('runner_c_compiler', 'gcc')
 
     @property
     def c_options(self) -> str:
@@ -29,18 +50,12 @@ class NeoRunner():
         Get the user defined options for the compiler or return empty string if not defined
         :return compiler options as str
         """
-        try:
-            return self.vim.api.get_var('runner_c_options')
-        except:
-            return ''
+        return self.__get_var_or_default('runner_c_options', '')
 
     @property
     def cpp_compiler(self) -> str:
         """return the user defined C++ compiler or the default if none exist"""
-        try:
-            return self.vim.api.get_var('runner_cpp_compiler')
-        except:
-            return 'g++'
+        return self.__get_var_or_default('runner_cpp_compiler', 'g++')
 
     @property
     def cpp_options(self) -> str:
@@ -48,27 +63,36 @@ class NeoRunner():
         Get the user defined options for the compiler or return empty string if not defined
         :return compiler options as str
         """
-        try:
-            return self.vim.api.get_var('runner_cpp_options')
-        except:
-            return ''
+        return self.__get_var_or_default('runner_cpp_options', '')
 
-    def __compile_and_run(self, filepath: str) -> None:
-        """determine file type based on file extension
-        :params
-            filepath: the file to check
+    @property
+    def python_interpreter(self) -> str:
+        """get the version of python the user wants"""
+        return self.__get_var_or_default('runner_python_ex', 'python3')
+
+    @property
+    def python_options(self) -> str:
+        return self.__get_var_or_default('runner_python_options', '')
+
+    def __make_binary_name(self) -> str:
         """
-        filetype: str = self.vim.eval('&filetype')
+        creates the binary name by removing the file extension
+        if the file is found /home/jondo/program.cc then it returns /home/jondo/program
+        """
+        return self.filepath.split('.')[0]
 
-        if filetype == 'python':
-            self.__run_python_script(filepath)
-        elif filetype == 'cpp':
-            self.__compile_c_cpp_file(self.cpp_compiler, filepath)
-        elif filetype == 'c':
-            self.__compile_c_cpp_file(self.c_compiler, filepath)
-        else:
-            self.vim.api.err_writeln(
-                f"NeoRunner doesn't support `{filetype}` file type")
+    def __runner_cmd(self, navigate: str, build: str, run: str) -> str:
+        """
+        creates a run command for the current buffer
+        :params
+            navigate: the command to go to the directory that has the file
+            build: the specific build command for the file
+            run: the command to execute binary
+        """
+        cmd = f"{navigate} && {build} && {run}"
+        # with open('/home/ben/repos/vim-runner/logfile.txt', 'w+') as f:
+        #     f.write(cmd)
+        return cmd
 
     def __run_command_in_terminal(self, command: str) -> None:
         """
@@ -80,64 +104,69 @@ class NeoRunner():
         new_win_height = win_height / 3
         self.vim.command(str(new_win_height) + "sp | term " + command)
 
-    def __make_binary_name(self, filepath: str):
-        """
-        creates the binary name by removing the file extension
-        if the file is found /home/jondo/program.cc then it returns /home/jondo/program
-        :params
-            filepath: the full path to the file
-        """
-        return filepath.split('.')[0]
+    def __get_c_or_cpp_properties(self) -> Tuple:
+        dictionary = {
+            'c': (self.c_compiler, self.c_options),
+            'cpp': (self.cpp_compiler, self.cpp_options),
+        }
+        return dictionary[self.filetype]
 
-    def __runner_cmd(self, navigate: str, build: str, run: str):
-        """
-        creates a run command for the current buffer
-        :params
-            navigate: the command to go to the directory that has the file
-            build: the specific build command for the file
-            run: the command to execute binary
-        """
-        cmd = f"{navigate} && {build} && {run}"
-        with open('/home/ben/repos/vim-runner/logfile.txt', 'w+') as f:
-            f.write(cmd)
-        return cmd
-
-    def __compile_c_cpp_file(self, compiler_to_use: str,  filepath: str, options: str = "") -> None:
+    def __compile_c_cpp_file(self) -> None:
         """
         compile a C/C++ file and run it non-interactively
         :params
             compiler_to_use: the compiler you wish to use (should be one of gcc, g++ or clang)
             filepath: the full path to the C/C++ file
         """
-        compiler = compiler_to_use
-        binaryname = self.__make_binary_name(filepath)
-        directory = os.path.dirname(filepath)
+        compiler, options = self.__get_c_or_cpp_properties()
+        binaryname = self.__make_binary_name()
+        directory = os.path.dirname(self.filepath)
 
         navigate = f"cd {directory}"
-        build = f"{compiler} {options} {filepath} -o {binaryname}"
+        build = f"{compiler} {options} {self.filepath} -o {binaryname}"
         run = binaryname
 
         cmd = self.__runner_cmd(navigate, build, run)
         self.__run_command_in_terminal(cmd)
 
-    def __run_python_script(self, filepath: str) -> None:
+    def __run_python_script(self) -> None:
         """
         Run a python file
         :params
             filepath: the full path to the python file
         """
-        python = "python3"
-        cmd = f"{python} {filepath}"
+        cmd = f"{self.python_interpreter} {self.python_options} {self.filepath}"
         self.__run_command_in_terminal(cmd)
+
+    @property
+    def func_dict(self) -> dict:
+        dictionary = {
+            'cpp': self.__compile_c_cpp_file,
+            'c': self.__compile_c_cpp_file,
+            'python': self.__run_python_script
+        }
+        return dictionary
+
+    def __compile_and_run(self) -> None:
+        """determine file type based on file extension
+        :params
+            filepath: the file to check
+        """
+
+        if self.filetype == 'python':
+            self.__run_python_script()
+        elif self.filetype == 'cpp':
+            self.__compile_c_cpp_file()
+        elif self.filetype == 'c':
+            self.__compile_c_cpp_file()
+        else:
+            self.vim.api.err_writeln(
+                f"NeoRunner doesn't support `{self.filetype}` file type")
 
     @pynvim.function('compile_current_file')
     def compile_current_file(self) -> None:
         """compile/run the current vim buffer if possible"""
-        filepath = self.vim.current.buffer.name
-
-        if filepath is None or filepath == "":
-            raise Exception(f"{filepath} is None")
-        self.__compile_and_run(filepath)
+        self.__compile_and_run()
 
     @pynvim.command('NeoRunner')
     def NeoRunner(self) -> None:
